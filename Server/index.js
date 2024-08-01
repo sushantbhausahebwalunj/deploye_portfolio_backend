2125dc8b525fcf04e7e25ce157291c348dc53be8
sushantbhausahebwalunj <sushantwalunj2001@gmail.com>
2024-08-01 14:59:03 +0530

    backend url updated

--- Server/index.js
+++ Server/index.js
@@ -32,6 +32,6 @@ app.get("/", (req, res) => {
 
 app.post("/send-mail", sendMail);
 
-app.listen(4000, () => {
-  console.log("Server is running on port 4000");
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running on port ${process.env.PORT}`);
 });
